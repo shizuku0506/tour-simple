89b81636b83a0e9201fae81f2f3c57f9ffb2e286
Park Jung Sun <js.park@sqisoft.com>
2018-12-14 18:11:06 +0900

    1

--- src/main/java/com/project/tour/TourApplication.java
+++ src/main/java/com/project/tour/TourApplication.java
@@ -1,18 +1,13 @@
 package com.project.tour;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.project.tour")
-public class TourApplication
-{
-	@Value("${property.hey}") private String propertyHey;
-
-	public static void main(String[] args)
-	{
-		SpringApplication.run(TourApplication.class, args);
-	}
+public class TourApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TourApplication.class, args);
+    }
 }
