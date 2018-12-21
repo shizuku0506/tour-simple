6d3676124329584fc543571842dbe46c0e9f789a
Park Jung Sun <js.park@sqisoft.com>
2018-12-21 18:22:50 +0900

    TDD 적용 준비중

--- src/main/java/com/project/tour/ProjectConstant.java
+++ src/main/java/com/project/tour/ProjectConstant.java
@@ -1,8 +1,11 @@
 package com.project.tour;
 
-public class ProjectConstant {
-
+public class ProjectConstant
+{
     public transient static final String REQ_ACCEPT_LANG = "ko";
 
     public transient static final String HEADER_ACCEPT_LANG = "accept-language";
+
+    public transient static final String TEST_LOCAL_URL = "http://localhost";
+    public transient static final int TEST_LOCAL_PROT = 8088;
 }
--- src/test/java/com/project/tour/TourApplicationTests.java
+++ src/test/java/com/project/tour/TourApplicationTests.java
@@ -1,17 +1,20 @@
 package com.project.tour;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
+@ActiveProfiles("local") // default value...  >> local
 public class TourApplicationTests
 {
-
-	@Test public void contextLoads()
-	{
-	}
-
+    @Test
+    public void contextLoads()
+    {
+    }
 }
