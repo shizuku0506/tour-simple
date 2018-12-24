c26343d9c4628742f22d85ee48f8d121070942a4
Park Jung Sun <js.park@sqisoft.com>
2018-12-24 17:26:23 +0900

    Content 브랜치 테스트1

--- src/main/java/com/project/tour/rest/ContentRestController.java
+++ src/main/java/com/project/tour/rest/ContentRestController.java
@@ -2,16 +2,12 @@ package com.project.tour.rest;
 
 import com.project.tour.domain.Content;
 import com.project.tour.service.ContentService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +26,47 @@ public class ContentRestController
             @ApiParam(value = "Accept-Language", required = true, defaultValue = "ko") @RequestHeader(value = "Accept-Language", defaultValue = "ko") String lang)
     {
         return contentService.getAllContent(lang);
+    }
+
+    @ApiOperation(value = "콘텐츠 조회 - 단일", notes = "<pre>콘텐츠 정보 하나를 조회한다.</pre>")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "seq", value = "콘텐츠 고유키(정수형의 숫자)", required = true, dataType = "string", paramType = "path", defaultValue = ""
+                    , example = "1"),
+    })
+    @GetMapping(value = "/{seq}")
+    public ResponseEntity getRegion(@PathVariable int seq)
+    {
+        return contentService.getContent(seq);
+    }
+
+    @ApiOperation(value = "콘텐츠 등록", notes = "<pre>콘텐츠 정보를 저장한다.</pre>")
+    @PostMapping(value = "",
+            consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE},
+            produces = {MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.TEXT_PLAIN_VALUE})
+    public ResponseEntity addRegion(@ApiParam(name = "region", value = "콘텐츠정보 데이터", required = true) @RequestBody Content content)
+    {
+        return contentService.addContent(content);
+    }
+
+    @ApiOperation(value = "콘텐츠 삭제", notes = "<pre>콘텐츠 정보를 삭제한다.</pre>")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "seq", value = "콘텐츠 고유키(정수형의 숫자)", required = true, dataType = "string", paramType = "path", defaultValue = "", example = "1")
+    })
+    @DeleteMapping(value = "/{seq}")
+    public ResponseEntity removeRegion(@PathVariable int seq)
+    {
+        return contentService.removeContent(seq);
+    }
+
+    @ApiOperation(value = "콘텐츠 수정", notes = "<pre>콘텐츠 정보를 수정한다.</pre>")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "seq", value = "콘텐츠 고유키(정수형의 숫자)", required = true, dataType = "string", paramType = "path", defaultValue = "", example = "")
+    })
+    @PatchMapping(value = "/{seq}", consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public ResponseEntity editRegion(@PathVariable int seq,
+                                     @ApiParam(name = "region", value = "콘텐츠정보 데이터", required = true) @RequestBody Content content)
+    {
+        content.setSeq(seq);
+        return contentService.editContent(content);
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
--- 코드 테이블 > 국가언어
+-- 코드 테이블 > 기초 데이터 (국가언어)
 insert into tb_code(group_id , code_cd , code_nm) values (100 , 'ko');
 insert into tb_code(group_id , code_cd , code_nm) values (100 , 'en');
 
@@ -9,3 +9,19 @@ insert into tb_region(title,lang) values ('미국','ko');
 insert into tb_region(title,lang) values ('USA','en');
 insert into tb_region(title,lang) values ('태국','ko');
 insert into tb_region(title,lang) values ('THAI','en');
+
+-- 컨텐츠 테이블 > 페이크 데이터
+insert into tb_content(title , description , lang ) values ('기초컨텐츠1' , '테스트 1' , 'ko');
+insert into tb_content(title , description , lang ) values ('기초컨텐츠2' , '테스트 2' , 'ko');
+insert into tb_content(title , description , lang ) values ('기초컨텐츠3' , '테스트 3' , 'ko');
+
+-- 스토리지 테이블 > 페이크 데이터
+insert into tb_storage(con_seq , file_size , file_path , file_nm, real_file_nm , file_ext , running_sec , file_status
+  , mime_tp )
+values ( 1 , 1000000 , 'D:/d_temp/123' ,'그림1' , '123' , 'jpg' , 0 , 'C' , 'image/jpg');
+insert into tb_storage(con_seq , file_size , file_path , file_nm, real_file_nm , file_ext , running_sec , file_status
+  , mime_tp )
+values ( 1 , 1000000 , 'D:/d_temp/123' ,'영상1' , '123' , 'wmv' , 30 , 'C' , 'video/wmv');
+insert into tb_storage(con_seq , file_size , file_path , file_nm, real_file_nm , file_ext , running_sec , file_status
+  , mime_tp )
+values ( 1 , 1000000 , 'D:/d_temp/123' ,'그림2' , '123' , 'jpg' , 0 , 'C' , 'image/jpg');
